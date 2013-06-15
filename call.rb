3c4cd8319981f41161bc91b204ef27208a2596de
Erik Schwartz <erik@tablexi.com>
2013-06-15 17:18:42 -0500

    Move call function outside of sinatra

--- call.rb
+++ call.rb
@@ -1,14 +1,6 @@
 require "rubygems"
-require "twilio-ruby"
 require "sinatra"
 
 get "/" do
-  # Get your Account Sid and Auth Token from twilio.com/user/account
-  account_sid = ENV.fetch("ACCOUNT_SID")
-  auth_token = ENV.fetch("AUTH_TOKEN")
-  @client = Twilio::REST::Client.new account_sid, auth_token
-  call = @client.account.calls.create(:url => "http://demo.twilio.com/docs/voice.xml",
-  :to => "+17738600685",
-  :from => "+17083254515")
-  puts call.sid
+
 end
